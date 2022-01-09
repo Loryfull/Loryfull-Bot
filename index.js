5387e0b70c4382b31f7697f57d84646b31a2c9ea
Loryfull <zanna.lory.100808@gmail.com>
2022-01-09 14:19:33 +0100

    Pache v.3.5
    Spero che sia la versione definitiva, nel caso abbandonerò il comando

--- index.js
+++ index.js
@@ -44,35 +44,25 @@ client.on("messageCreate", message => {
             message.channel.send ({ embeds: [embed] })
      }
 })
-client.on("messageCreate", (message) => {
-    if (message.content == "l/ServerInfo") {
-
-        var server = message.member.guilds 
-
-        var botCount =  server.members.cache.filter(member => member.user.bot).size;
+client.on("message", message => {
+    if (message.content == "!serverinfo") {
+        var server = message.member.guild;
+        var botCount = server.members.cache.filter(member => member.user.bot).size;
         var utentiCount = server.memberCount - botCount;
-
-        var categoryCuont = server.channels.cache.filter(c => c.type == "category").size;
-        var textCuont = server.channels.cache.filter(c => c.type == "text").size;
-        var voiceCuont = server.channels.cache.filter(c => c.type == "voice").size;
-
+        var categoryCount = server.channels.cache.filter(c => c.type == "category").size
+        var textCount = server.channels.cache.filter(c => c.type == "text").size
+        var voiceCount = server.channels.cache.filter(c => c.type == "voice").size
         var embed = new Discord.MessageEmbed()
-
-             .setTitle (server.name)
-             .setDescription ("Le informazioni essenziali su questo server")
-             .setThumbnail (server.iconURL())
-             .addField ("Owner", server.owner.user.username, true)
-             .addField ("Server ID", server.id, true)
-             .addField ("Server Region", server.region, true)
-             .addField ("Members", "Total: " + server.memberCount + " -User: " + utentiCount + " -Bot: " + botCount, true)
-             .addField ("Channels", "category: " + categoryCuont + " -text: " + textCuont + " -voice: " + voiceCuont, true)
-             .addField ("Server Created", server.createdAt.toDateString(), true)
-             .addField ("Boost Level", "Level " + server.premiumTier + "(Boost: " + server.premiumSubscriptionCount + ")", true)  
-            
-         message.channel.send ({ embeds: [embed] })
-            
-     }     
- })
-
-
- 
+            .setTitle(server.name)
+            .setDescription("Tutte le info su questo server")
+            .setThumbnail(server.iconURL())
+            .addField("Owner", server.owner.user.username, true)
+            .addField("Server id", server.id, true)
+            .addField("Server region", server.region, true)
+            .addField("Members", "Total: " + server.memberCount + " - Users: " + utentiCount + " - Bots: " + botCount, false)
+            .addField("Channels", "Category: " + categoryCount + " - Text: " + textCount + " - Voice: " + voiceCount, false)
+            .addField("Server created", server.createdAt.toDateString(), true)
+            .addField("Boost level", "Level " + server.premiumTier + " (Boost: " + server.premiumSubscriptionCount + ")", true)
+        message.channel.send(embed)
+    }
+})
